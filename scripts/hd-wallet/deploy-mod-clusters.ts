4d40f10d64cf82fe0329e212ad8e1f4ddd552292
Marco <matabasco@gmail.com>
2023-06-14 14:01:22 +0200

    hd_wallet support

---
+++ scripts/hd-wallet/deploy-mod-clusters.ts
@@ -0,0 +1,35 @@
+import { ethers } from 'hardhat';
+const { LedgerSigner } = require("@ethersproject/hardware-wallets");
+
+import ssvModules from '../modules/ssvModules';
+
+async function deploy() {
+  const proxyAddress = process.env.SSVNETWORK_PROXY_ADDRESS;
+  if (!proxyAddress) throw new Error("SSVNETWORK_PROXY_ADDRESS not set.");
+
+  const provider = new ethers.providers.JsonRpcProvider(process.env.GOERLI_ETH_NODE_URL);
+  const signer = new LedgerSigner(provider);
+  console.log(`Deploying contracts with the account:${await signer.getAddress()}`);
+
+  // Initialize contract
+  const ssvNetworkFactory = await ethers.getContractFactory('SSVNetwork', signer);
+  const ssvClustersModFactory = await ethers.getContractFactory('SSVClusters', signer);
+
+  // Deploy ssvClustersMod
+  const ssvClustersMod = await ssvClustersModFactory.deploy();
+  await ssvClustersMod.deployed();
+  console.log(`SSVClusters module deployed to: ${ssvClustersMod.address}`);
+
+  const ssvNetwork = await ssvNetworkFactory.attach(proxyAddress);
+
+  await ssvNetwork.upgradeModule(ssvModules.SSV_CLUSTERS, ssvClustersMod.address);
+  console.log(`SSVClsuters module attached to SSVNetwork succesfully`);
+
+}
+
+deploy()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
