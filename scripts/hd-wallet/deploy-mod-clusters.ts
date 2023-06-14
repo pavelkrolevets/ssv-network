d0349cc6748e02926111da5de459ad692201a434
Marco <matabasco@gmail.com>
2023-06-14 14:09:03 +0200

    remove SSVNetwork link

--- scripts/hd-wallet/deploy-mod-clusters.ts
+++ scripts/hd-wallet/deploy-mod-clusters.ts
@@ -4,15 +4,11 @@ const { LedgerSigner } = require("@ethersproject/hardware-wallets");
 import ssvModules from '../modules/ssvModules';
 
 async function deploy() {
-  const proxyAddress = process.env.SSVNETWORK_PROXY_ADDRESS;
-  if (!proxyAddress) throw new Error("SSVNETWORK_PROXY_ADDRESS not set.");
-
   const provider = new ethers.providers.JsonRpcProvider(process.env.GOERLI_ETH_NODE_URL);
   const signer = new LedgerSigner(provider);
   console.log(`Deploying contracts with the account:${await signer.getAddress()}`);
 
   // Initialize contract
-  const ssvNetworkFactory = await ethers.getContractFactory('SSVNetwork', signer);
   const ssvClustersModFactory = await ethers.getContractFactory('SSVClusters', signer);
 
   // Deploy ssvClustersMod
